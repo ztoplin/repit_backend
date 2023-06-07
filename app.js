1c3a86a52a65c33a8114d7e359874f03abbf247b
Aaron <ztoplin@yahoo.com>
2023-06-07 19:24:52 -0400

    fixed update offices

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
+  console.log('IN APP/MIDDLEWARE', req.headers, req.body)
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("X-Frame-Options", "sameorigin");
   res.header("Referrer-Policy", "no-referrer-when-downgrade");
